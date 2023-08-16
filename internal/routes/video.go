6ee1a231064071160889adaef53ee9c02c0e6b21
souvik150 <souvikmukherjee150@gmail.com>
2023-08-16 23:39:25 +0530

    fix: file structure cleanup

--- internal/routes/video.go
+++ internal/routes/video.go
@@ -7,6 +7,12 @@ import (
 
 func VideoRoutes(group fiber.Router) {
 	videoGroup := group.Group("/video")
+
+	// These are authenticated routes
+	//videoGroup.Get("/", middleware.TokenValidation, controllers.GetVideos)
+	//videoGroup.Post("/:topic", middleware.TokenValidation, controllers.FetchAndStoreVideos)
+
+	// For the purpose of this task user without auth is also allowed
 	videoGroup.Get("/", controllers.GetVideos)
 	videoGroup.Post("/:topic", controllers.FetchAndStoreVideos)
 }
--- internal/worker/youtube.go
+++ internal/worker/youtube.go
@@ -1,1 +1,38 @@
 package worker
+
+import (
+	initializers "github.com/souvik150/Fampay-Backend-Assignment/config"
+	"github.com/souvik150/Fampay-Backend-Assignment/internal/services"
+	"log"
+	"time"
+)
+
+func StartVideoWorker(topic string) {
+	config, _ := initializers.LoadConfig(".")
+	apiKeys := config.APIKeysYT
+
+	keyIndex := 0
+
+	go func() {
+		for {
+			apiKey := apiKeys[keyIndex]
+
+			videos, err := services.CallYouTubeAPI(apiKey, topic)
+			if err != nil {
+				log.Printf("Error fetching videos: %v", err)
+				// Switch to the next available API key if quota is exhausted
+				keyIndex = (keyIndex + 1) % len(apiKeys)
+				continue
+			}
+
+			for i := range videos {
+				err := services.SaveVideo(videos[i])
+				if err != nil {
+					log.Println("Error storing videos:", err)
+				}
+			}
+
+			time.Sleep(30 * time.Second)
+		}
+	}()
+}
